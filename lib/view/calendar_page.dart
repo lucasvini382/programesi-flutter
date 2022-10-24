e67c1ceec39dc7c323e8213bab2c2b885ebf3176
Lucas Oliveira <lucas.sales@justa.com.vc>
2022-10-23 23:02:24 -0300

    feat: :sparkles: adiciona tela de registro de data/hora

--- lib/view/calendar_page.dart
+++ lib/view/calendar_page.dart
@@ -1,6 +1,7 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:programesi/utils/routing/routes.dart';
 import 'package:programesi/view_model/user.view_model.dart';
 
 class CalendarPage extends StatefulWidget {
@@ -39,15 +40,40 @@ class _CalendarPageState extends State<CalendarPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: ListView.builder(
-          itemCount: userViewModel.timeworks!.length,
-          itemBuilder: (context, index) {
-            String teste = userViewModel.timeworks![0].description!;
-            return ListTile(
-              title: Text('item $teste'),
-            );
-          }),
+    return Scaffold(
+      floatingActionButton: ElevatedButton(
+          onPressed: () {
+            Modular.to.pushNamed(registerRoute);
+          },
+          child: const Text('Adicionar horário')),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Material(
+        color: Colors.grey.shade200,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+              itemCount: userViewModel.timeworks!.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Text(userViewModel.timeworks![0].description!),
+                    SizedBox(
+                        height: 2 * MediaQuery.of(context).devicePixelRatio),
+                    Text(userViewModel.timeworks![0].calendars![0].date!),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(userViewModel.timeworks![0].entryHour!),
+                        SizedBox(
+                            width: 2 * MediaQuery.of(context).devicePixelRatio),
+                        Text(userViewModel.timeworks![0].exitHour!),
+                      ],
+                    ),
+                  ],
+                );
+              }),
+        ),
+      ),
     );
   }
 }
