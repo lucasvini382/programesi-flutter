5a7e7aef451fc8eda7c59c8ac7e2fb02e411b7dd
Lucas Oliveira <lucasvini382@gmail.com>
2022-11-23 22:12:25 -0300

    feature: :sparkles: cria pagina do onboarding.

--- lib/utils/routing/modules/app.module.dart
+++ lib/utils/routing/modules/app.module.dart
@@ -2,6 +2,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:programesi/utils/routing/routes.dart';
 import 'package:programesi/view/calendar_page.dart';
 import 'package:programesi/view/login_page.dart';
+import 'package:programesi/view/onboarding_page.dart';
 import 'package:programesi/view/register_page.dart';
 import 'package:programesi/view_model/user.view_model.dart';
 
@@ -13,10 +14,17 @@ class AppModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-        ChildRoute(loginRoute, child: (context, args) => const LoginPage()),
+        ChildRoute(loginRoute,
+            transition: TransitionType.rightToLeft,
+            child: (context, args) => const LoginPage()),
+        ChildRoute(onboardingRoute,
+            transition: TransitionType.rightToLeft,
+            child: (context, args) => const OnboardingPage()),
         ChildRoute(calendarRoute,
+            transition: TransitionType.rightToLeft,
             child: (context, args) => const CalendarPage()),
         ChildRoute(registerRoute,
+            transition: TransitionType.rightToLeft,
             child: (context, args) => const RegisterPage()),
       ];
 }
